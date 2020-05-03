df7552342fe348607518e454cc549e652892ba91
Kerber <DaIvan97@gmail.com>
2020-05-03 17:57:49 +0300

    Add project files.

---
+++ Source/BethesdaModule ShellView.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "BethesdaModule ShellView.h"
+
+int APIENTRY wWinMain(HINSTANCE hInstance,
+					  HINSTANCE hPrevInstance,
+					  LPWSTR lpCmdLine,
+					  int nCmdShow)
+{
+	return 0;
+}
---
+++ stdafx.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <SDKDDKVer.h>
+#include <windows.h>
